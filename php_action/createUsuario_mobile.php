85a11d36e7c8b5fc8a093afb7284d3e9fa452763
Dakkin <mouradavi12@gmail.com>
2021-04-12 15:04:12 -0300

    create usuario mobile

---
+++ php_action/createUsuario_mobile.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'db_connect.php';
+
+$response = array();
+if (isset($_POST['login']) && isset($_POST['senha']) && isset($_POST['email']) && isset($_POST['telefone']) && isset($_POST['nome']) && isset($_POST['data_nascimento'])){
+    $login = trim($_POST['login']);
+    $senha = trim($_POST['senha']);
+    $telefone = trim($_POST['telefone']);
+    $email = trim($_POST['email']);
+    $nome = trim($_POST['nome']);
+    $data_nascimento = trim($_POST['data_nascimento']);
+    
+
+    $result = pg_query($connect, "INSERT INTO usuario (nome, data_nascimento, email, telefone, login, senha) VALUES ('$nome','$data_nascimento','$email', '$telefone', '$login', $senha)";
+		if ($result) {
+			$response["success"] = 1;
+		}
+		else {
+			$response["success"] = 0;
+        }
+
+pg_close($connect);
+echo json_encode($response);
+
+
+
+?>
