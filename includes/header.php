43aed97041c02c479242b1443f6f3df1a6e0a54a
Dakkin <mouradavi12@gmail.com>
2021-04-12 10:52:04 -0300

    barra

--- includes/header.php
+++ includes/header.php
@@ -13,29 +13,27 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
-    <body>
+    <body> 
+    
 
     <nav>
   <a href="#!" class="brand-logo">MusicStation</a><br>
         <div class="nav-wrapper grey darken-1">
             
             <ul class="right hide-on-med-and-down">
+                
                 <li><a href="index.php">Início</a></li>
+                <?php
+                if (!isset($_SESSION['login']) and !isset($_SESSION['senha']) )
+{?>
                 <li><a href="anuncio.php">Meu Anúncio</a></li>
                 <li><a href="contratarEvento.php">Contratar evento</a></li>
                 <li><a href="meusEventos.php">Meus Eventos</a></li>
                 <li><a href="meusContratos.php">Meus Contratos</a></li>
                 <li><a href="usuario.php">Perfil</a></li>
+  <?php} ?>
               
-            </ul>
-            <!--
-            <ul class="side-nav" id="mobile-demo">
-            <li><a href="index.php">Início</a></li>
-                <li><a href="anuncio.php">Meu Anúncio</a></li>
-                <li><a href="contratarEvento.php">Contratar evento</a></li>
-                <li><a href="meusEventos.php">Meus eventos</a></li>
-            </ul>
-            -->
+            </ul>         
         </div>
     </nav>  
     <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
