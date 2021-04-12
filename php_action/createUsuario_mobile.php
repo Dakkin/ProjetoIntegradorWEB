e8676b9e8b7f184489fe2c66d080b662eea699e2
Dakkin <mouradavi12@gmail.com>
2021-04-12 15:36:15 -0300

    dadad

--- php_action/createUsuario_mobile.php
+++ php_action/createUsuario_mobile.php
@@ -8,7 +8,8 @@ if (isset($_POST['login']) && isset($_POST['senha']) && isset($_POST['email']) &
     $telefone = trim($_POST['telefone']);
     $email = trim($_POST['email']);
     $nome = trim($_POST['nome']);
-    $data_nascimento = trim($_POST['data_nascimento']);
+    $data_nascimento = pg_escape_string($connect, date('Y-m-d', strtotime($_POST["data_nascimento"])));    
+
     
 
     $result = pg_query($connect, "INSERT INTO usuario (nome, data_nascimento, email, telefone, login, senha) VALUES ('$nome','$data_nascimento','$email', '$telefone', '$login','$senha')");
