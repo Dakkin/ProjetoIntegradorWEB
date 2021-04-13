29cc0ba9f910fcf44180b82cb37f624f713d93ff
Dakkin <mouradavi12@gmail.com>
2021-04-13 01:02:47 -0300

    detalhes

---
+++ php_action/createAnuncio_mobile.php
@@ -0,0 +1,30 @@
+<?php
+//Conexão
+require_once 'db_connect.php';
+$response = array();
+if (isset($_POST['descricao']) && isset($_POST['estilo']) && isset($_POST['spotify']) && isset($_POST['instrumentos']) && isset($_POST['cache_minimo']) && isset($_FILES['foto']) && isset($_POST['login'])):
+    $descricao = pg_escape_string($connect, $_POST['descricao']);
+    $estilo = (int)pg_escape_string($connect, $_POST['estilo']);
+    $spotify = pg_escape_string($connect, $_POST['spotify']);
+    $instrumentos = pg_escape_string($connect, $_POST['instrumentos']);
+    $cache_minimo = floatval(pg_escape_string($connect, $_POST['cache_minimo']));
+    $login = pg_escape_string($connect, $_POST['login']);
+   
+
+
+    $imageFileType = strtolower(pathinfo(basename($_FILES["foto"]["name"]),PATHINFO_EXTENSION));
+    $image_base64 = base64_encode(file_get_contents($_FILES['foto']['tmp_name']));
+    $img = 'data:image/'.$imageFileType.';base64,'.$image_base64;
+    
+    $sql = "INSERT INTO anuncio (estilo, spotify, instrumentos, cache_minimo, descricao, foto, usuario_login) VALUES ('$estilo','$spotify','$instrumentos', '$cache_minimo','$descricao','$img','$login')";
+    if (pg_query($connect, $sql)):
+        $response['sucesso'] = 1;
+    else:
+        $response['sucesso'] = 0;
+        $response['erro'] = "Não foi possível adicionar o anúncio";
+    endif;
+else:
+    $response['sucesso'] = 0;
+    $response['erro'] = "Faltam parâmetros no anúncio";
+endif; 
+?>
---
+++ php_action/detalhesAnuncio_mobile.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'db_connect.php';
+$response = array();
+if (isset($_GET['idanuncio'])){
+    $idanuncio = trim($_GET['idanuncio']);
+    $sql = "SELECT * FROM anuncio WHERE idanuncio = '$idanuncio'";
+    $result = pg_query($connect, $sql);
+    if(pg_num_rows($result) > 0){
+        $row = pg_fetch_array($result);
+        $response["anuncio"] = array();    
+        $anuncio = array();
+        $anuncio["estilo"] = $row['estilo'];
+        $anuncio['spotify'] = $row['spotify'];
+        $anuncio['instrumentos'] = $row['instrumentos'];
+        $anuncio['cache_minimo'] = $row['cache_minimo'];
+        $anuncio['descricao'] = $row['descricao'];
+        $anuncio['foto'] = $row['foto'];
+        $sqlusuario = "SELECT email FROM usuario WHERE login = $row['usuario_login']"
+        $resultUsuario = pg_query($connect, $sqlusuario);
+        $rowUsuario = pg_fetch_array($resultUsuario);
+        $anuncio['email'] = $rowUsuario['email'];
+
+
+        array_push($response["anuncio"], $anuncio);
+    
+        $response["sucesso"] = "1";
+    }
+    else{
+        $response['sucesso'] = 0;
+        $response['erro'] = "Não foi possivel obter os dados do anúncio";
+    } 
+}
+else {
+    $response['sucesso'] = 0;
+    $response['erro'] = "Não foi passado o id do anúncio";
+}
+pg_close($connect);
+
+echo json_encode($response);
+
+?>
