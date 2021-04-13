8b50cbcae09de7e19809bb2bb591a6b5216dfa73
Dakkin <mouradavi12@gmail.com>
2021-04-12 22:34:55 -0300

    dale

---
+++ php_action/usuario_mobile.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'db_connect.php';
+$response = array();
+if (isset($_POST['login']){
+    $login = trim($_POST['login']);
+    $sql = "SELECT * FROM usuario WHERE login = $login"
+}
+$result = pg_query($connect, $sql);
+if(pg_num_rows($result) > 0){
+    $response["usuario"] = array();    
+    $usuario = array();
+    $anuncio["data_nascimento"] = $row['data_nascimento'];
+    $anuncio['nome'] = $row['nome'];
+    $anuncio['email'] = $row['email'];
+    $anuncio['telefone'] = $row['telefone'];   
+
+    array_push($response["usuario"], $usuario);
+    
+    
+    $response["sucesso"] = "1";
+
+    pg_close($connect);
+
+    echo json_encode($response);
+
+}else {
+    $response['sucesso'] = 0;
+
+    pg_close($connect);
+
+    echo json_encode($response);
+}
+?>
