bb6ad055dac3b888171c24bbf746339d8e8041c5
Dakkin <mouradavi12@gmail.com>
2021-04-13 00:09:41 -0300

    sdad

--- php_action/usuario_mobile.php
+++ php_action/usuario_mobile.php
@@ -5,31 +5,31 @@ if (isset($_GET['login'])){
     error_log($_GET['login']);
     $login = trim($_GET['login']);
     $sql = "SELECT * FROM usuario WHERE login = '$login'";
+    $result = pg_query($connect, $sql);
+    if(pg_num_rows($result) > 0){
+        $row = pg_fetch_array($result);
+        $response["usuario"] = array();    
+        $usuario = array();
+        $usuario["data_nascimento"] = $row['data_nascimento'];
+        $usuario['nome'] = $row['nome'];
+        $usuario['email'] = $row['email'];
+        $usuario['telefone'] = $row['telefone'];   
+
+        array_push($response["usuario"], $usuario);
+    
+        $response["sucesso"] = "1";
+    }
+    else{
+        $response['sucesso'] = 0;
+        $response['erro'] = "Não foi possivel obter os dados do usuário";
+    } 
 }
-$result = pg_query($connect, $sql);
-if(pg_num_rows($result) > 0){
-    $row = pg_fetch_array($result);
-    $response["usuario"] = array();    
-    $usuario = array();
-    $usuario["data_nascimento"] = $row['data_nascimento'];
-    $usuario['nome'] = $row['nome'];
-    $usuario['email'] = $row['email'];
-    $usuario['telefone'] = $row['telefone'];   
-
-    array_push($response["usuario"], $usuario);
-    
-    
-    $response["sucesso"] = "1";
-
-    pg_close($connect);
-
-    echo json_encode($response);
-
-}else {
+else {
     $response['sucesso'] = 0;
-
-    pg_close($connect);
-
-    echo json_encode($response);
+    $response['erro'] = "Não foi passado o login do usuário";
 }
+pg_close($connect);
+
+echo json_encode($response);
+
 ?>
