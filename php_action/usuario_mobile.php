511c3b5988d1c6c50d08a49265f1d83eedfe223e
Dakkin <mouradavi12@gmail.com>
2021-04-12 23:13:05 -0300

    sem row

--- php_action/usuario_mobile.php
+++ php_action/usuario_mobile.php
@@ -10,10 +10,10 @@ $result = pg_query($connect, $sql);
 if(pg_num_rows($result) > 0){
     $response["usuario"] = array();    
     $usuario = array();
-    $usuario["data_nascimento"] = $row['data_nascimento'];
-    $usuario['nome'] = $row['nome'];
-    $usuario['email'] = $row['email'];
-    $usuario['telefone'] = $row['telefone'];   
+    $usuario["data_nascimento"] = ['data_nascimento'];
+    $usuario['nome'] = ['nome'];
+    $usuario['email'] = ['email'];
+    $usuario['telefone'] = ['telefone'];   
 
     array_push($response["usuario"], $usuario);
     
